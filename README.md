73d3139489f4c7deb8fa3128fcf9f2e110f6e3cc
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2020-04-22 12:54:37 +0200

    add singular perturbation example

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
 # Simple NGSolve examples from binder
 
+Poisson example:
 [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/schruste/ngsolve-binder-examples/master?filepath=poisson.ipynb)
+
+Singular perturbation example:
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/schruste/ngsolve-binder-examples/master?filepath=singular_perturb.ipynb)
--- draw.py
+++ draw.py
@@ -1,20 +1,35 @@
 #from pyvista import set_plot_theme
 #set_plot_theme('document')
 
-from ngsolve import VTKOutput
+from ngsolve import VTKOutput, CoefficientFunction, Mesh
 
 # from IPython import get_ipython
 # ipython = get_ipython()
 # ipython.magic("matplotlib inline")
 
 import pyvista as pv
-def PyVistaDraw(gfu, mesh, name, sd=0, filename="tmp"):
-    if gfu.dim > 1:
-        raise Exception("no vectorial things yet")
-    vtkout = VTKOutput(mesh, [gfu], [name], 
-                       filename = filename, subdivision=sd)
-    vtkout.Do()
-    plot_mesh = pv.read(filename+".vtk")
-    p = pv.Plotter(notebook=True)
-    p.add_mesh(plot_mesh.warp_by_scalar(name,normal=(0,0,1),factor=1), scalars=name, label=name) 
-    return p.show(title=name,use_panel=True, auto_close=False)    
+def PyVistaDraw(obj, mesh=None, name="dummy", sd=0, filename="tmp", static=False):
+    if isinstance(obj, CoefficientFunction):
+        gfu = obj
+        if mesh == None:
+            mesh = gfu.space.mesh
+        if gfu.dim > 1:
+            raise Exception("no vectorial things yet")
+        vtkout = VTKOutput(mesh, [gfu], [name], 
+                           filename = filename, subdivision=sd)
+        vtkout.Do()
+        plot_mesh = pv.read(filename+".vtk")
+        p = pv.Plotter(notebook=True)
+        p.add_mesh(plot_mesh.warp_by_scalar(name,normal=(0,0,1),factor=1), scalars=name, label=name) 
+        return p.show(title=name,use_panel=not static, auto_close=False)    
+    elif isinstance(obj,Mesh):
+        mesh = obj
+        vtkout = VTKOutput(mesh, [], [], 
+                           filename = filename, subdivision=0)
+        vtkout.Do()
+        plot_mesh = pv.read(filename+".vtk")
+        p = pv.Plotter(notebook=True)
+        p.add_mesh(plot_mesh, show_edges=True) 
+        return p.show(title=name,use_panel=not static, auto_close=False)
+    else:
+        raise Exception("obj-type is not viable")
