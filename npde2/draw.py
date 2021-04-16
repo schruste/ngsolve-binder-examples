b9d74fd8fe3f1b76ee2999c555cd197edf0a069e
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-04-17 00:01:04 +0200

    binder using docker / draw without pyvista

--- npde2/draw.py
+++ npde2/draw.py
@@ -6,63 +6,6 @@ from ngsolve import VTKOutput, CoefficientFunction, Mesh
 # ipython = get_ipython()
 # ipython.magic("matplotlib inline")
 try:
-    import pyvista as pv
-    def PyVistaDraw(obj, mesh=None, name="dummy", sd=0, filename="tmp", elevation = 1, static=False, show_edges = True):
-        if isinstance(obj, CoefficientFunction):
-            gfu = obj
-            if mesh == None:
-                mesh = gfu.space.mesh
-            if gfu.dim == 2:
-                gfu = CoefficientFunction((gfu[0],gfu[1],0))
-
-            vtkout = VTKOutput(mesh, [gfu], [name], 
-                               filename = filename, subdivision=sd)
-            vtkout.Do()
-            plot_mesh = pv.read(filename+".vtk")
-
-            
-            p = pv.Plotter(notebook=True)
-
-            if gfu.dim == 1 and elevation != 0:
-                plot_mesh = plot_mesh.warp_by_scalar(name,normal=(0,0,1),factor=elevation)
-
-            p.add_mesh(plot_mesh, scalars=name, label=name)
-
-            if show_edges:
-                edges = plot_mesh.extract_feature_edges(20)
-                p.add_mesh(edges, color="black") 
-            
-            if gfu.dim > 1:
-                glyphs = plot_mesh.glyph(orient=name, scale=name, factor=1, geom=pv.Arrow())
-                p.add_mesh(glyphs, color="black")
-
-            return p.show(title=name,use_panel=not static, auto_close=False)
-        elif isinstance(obj,Mesh):
-            mesh = obj
-            vtkout = VTKOutput(mesh, [], [], 
-                               filename = filename, subdivision=sd)
-            vtkout.Do()
-            plot_mesh = pv.read(filename+".vtk")
-            p = pv.Plotter(notebook=True)
-            edges = plot_mesh.extract_feature_edges(20)
-            p.add_mesh(plot_mesh, show_edges=False) 
-            p.add_mesh(edges, color="black") 
-            return p.show(title=name,use_panel=not static, auto_close=False)
-        else:
-            raise Exception("obj-type is not viable")
-        
-    # Draw = PyVistaDraw
-    print("* PyVista-Drawing with 'PyVistaDraw' available")
-except ImportError:
-    try:
-        from ngsolve.webgui import Draw
-    except ImportError:
-        from netgen import gui
-        from ngsolve import Draw
-        print("Spawning NGSolve's GUI with standard 'Draw' function")
-    print("Using NGSolve's 'Draw' from webgui!")
-
-try:
     from ngsolve.webgui import Draw
     print("Using NGSolve's 'Draw' from webgui!")
 except ImportError:
@@ -135,8 +78,12 @@ def Draw1D(mesh, coefs, keep=False, n_p=2, figsize=(20,4)):
 print("* 1D Drawing available with patched 'Draw' or simply 'Draw1D'")
 
 oldDraw = Draw
-def Draw(cf,mesh,label,*args,**kwargs):
-    if mesh.dim == 1:
-        Draw1D(mesh,[(cf,label)],*args,**kwargs)
+def Draw(cf_or_mesh,mesh=None,label=None,*args,**kwargs):
+    if mesh == none:
+        oldDraw(cf_or_mesh)
     else:
-        oldDraw(cf,mesh,label,*args,**kwargs)
+        cf = cf_or_mesh
+        if mesh.dim == 1:
+            Draw1D(mesh,[(cf,label)],*args,**kwargs)
+        else:
+            oldDraw(cf,mesh,label,*args,**kwargs)
